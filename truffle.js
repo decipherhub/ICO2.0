cb58f0f7087365219cf5f22d370f258928764af0
JhChoy <jchoyeclipse@gmail.com>
2018-05-10 15:50:54 +0900

    update module

--- truffle.js
+++ truffle.js
@@ -1,3 +1,7 @@
+require('babel-register')({
+    ignore: /node_modules\/(?!openzeppelin-solidity\/test\/helpers)/
+});
+require('babel-polyfill');
 module.exports = {
     networks: {
         development: {
