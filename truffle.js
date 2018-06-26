349ed317bf071c88d8d9810af7b6d0d74cd3e51a
jw-pyo <wjddn1801@snu.ac.kr>
2018-06-26 15:02:49 +0900

    add gasPrice to 0

--- truffle.js
+++ truffle.js
@@ -7,7 +7,8 @@ module.exports = {
         development: {
             host: "127.0.0.1",
             port: 8545,
-            network_id: "*"
+            network_id: "*",
+            gasPrice: 0 // this is necessary for dev
         }
     }
 };
